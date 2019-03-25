aa955c2ab04af951d448a4a39c687ae6b51b1064
Alexander Mandera <alexander@mandera.eu>
2019-03-25 18:38:27 +0100

    Add entry point for other modules to get classes easily

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const Core = require('Core.class');
+const Module = require('./module/Module.class');
+const ModuleLoader = require('./module/ModuleLoader.class');
+const Log = require('./util/Log.class');
 
 global.__basedir = __dirname;
-module.exports = Core;
+module.exports = {
+    Core, Module, ModuleLoader, Log
+};
